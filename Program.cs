065982c6383b3179c7f84ec3ba9b1318352ac4af
Simone Sada <si.sada93@gmail.com>
2022-06-27 14:22:56 +0200

    Complete snack 3

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// SNACK 3
+Console.WriteLine("\n*** SNACK 3 *** ");
+
+int inputNum = 10;  // numero di input richesti all'utente
+int[] array = new int[inputNum]; // array che salva gli input dell'utente
+int sum = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    // si potrebbe aggiungere un controllo per verificare che l'input inserito sia nel formato richiesto
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    sum += array[i];
+}
+
+Console.WriteLine($"\nSum: {sum}");
+
